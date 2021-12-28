4137ddcf05ecfe2c66d0ee06b04c3f8bc67d7cf3
Firdaws Lamidi <74219529+Fificode@users.noreply.github.com>
2021-12-28 11:52:36 +0100

    Update login.js

--- JS/login.js
+++ JS/login.js
@@ -4,75 +4,31 @@ const logInForm = document.getElementById("login-form");
 const emailInput = document.getElementById("email-login");
 const passwordInput = document.getElementById("password-login");
 
-// const submitLogin = async(e) => {
-//     e.preventDefault();
-//    let response = await fetch(“http://localhost:8001/login”, { 
-//    method: "POST",
-//     headers: {
-//    Accept: "application/json, text/plain, */*",
-//     "Content-Type": "application/json", 
-//    },
-//     body: JSON.stringify({
-//     Email: emailInput.value,
-//     password: passwordInput.value, 
-//    }),
-// });
-   
-//    .then((response) => {
-//         let data = response.data;
-//    localStorage.setItem("userdata",JSON.stringify(data));
-//    if (data.usertype=='student') {
-//     Window.location.href = “user-account.html" }
-//      else {Window.location.href = “publishpaper.html"
-//     }
-//    }
-//    .catch(
-//    (err) =>
-//     { console.log(err); }
-//    );
-   
-// logInForm.addEventListener('submit', submitLogIn);
-const submitLogin = (e) => {
+var submitLogin = (e) => {
     e.preventDefault();
-    fetch(“http://localhost:8001/login”, { 
+    fetch("http://localhost:8001/login", { 
    method: "POST",
     headers: {
-   Accept: "application/json, text/plain, */*",
+   Accept: "application/json, text/plain, /",
     "Content-Type": "application/json", 
    },
     body: JSON.stringify({
     Email: emailInput.value,
     password: passwordInput.value, 
-   }),
-   
+   })})
    .then((response) => {
-       
-   let data = response.data;
-   
-   
-   
-   
-           localStorage.setItem("userdata",JSON.stringify(data));
-   
-   
-              if (data.usertype=='student') {
-   
-   
-                  Window.location.href = “user-account.html"
-   
-   
-              } else {
-   
-   
-                  Window.location.href = “publishpaper.html"
-   
-   
-              }
-   
-   
-   }
-   catch(
+       let data = response.data;
+   localStorage.setItem("userdata",JSON.stringify(data));
+    if (data.usertype=='student') {
+  Window.location.href = "user-account.html"
+  } else {
+   Window.location.href = "publishpaper.html"
+ }
+    })
+   .catch(
    (err) =>
     { console.log(err); }
    );
-   
+    }
+
+ logInForm.addEventListener('submit', submitLogIn);
